207472c70af4359d701d7ed0695f0b64550424cf
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2018-04-03 00:28:39 +0200

    Complete refactor for current logic.

--- GildedRose.playground/Pages/Tests.xcplaygroundpage/Sources/GuildedRose.swift
+++ GildedRose.playground/Pages/Tests.xcplaygroundpage/Sources/GuildedRose.swift
@@ -38,13 +38,6 @@ public class GildedRose {
         for i in 0..<items.count {
 
             switch items[i].name {
-            case "Normal":
-                items[i] = items[i].changeSellIn(increment: -1)
-                if items[i].sellIn >= 0 {
-                    items[i] = items[i].changeQuality(increment: -1).limitQualityTo(min: 0)
-                } else {
-                    items[i] = items[i].changeQuality(increment: -2).limitQualityTo(min: 0)
-                }
 
             case "Aged Brie":
                 items[i] = items[i].changeSellIn(increment: -1)
@@ -71,54 +64,12 @@ public class GildedRose {
                 }
 
             default:
-
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
+                items[i] = items[i].changeSellIn(increment: -1)
+                if items[i].sellIn >= 0 {
+                    items[i] = items[i].changeQuality(increment: -1).limitQualityTo(min: 0)
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    items[i] = items[i].changeQuality(increment: -2).limitQualityTo(min: 0)
                 }
-            }
             }
         }
     }
